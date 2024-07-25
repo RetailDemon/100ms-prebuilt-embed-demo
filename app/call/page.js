c4227e8ab1445ecef53eb608fdd6aad43e4a3388
RETDEM LLC. <154280502+RetailDemon@users.noreply.github.com>
2024-07-25 14:09:27 -0400

    Update page.js

--- app/call/page.js
+++ app/call/page.js
@@ -4,7 +4,7 @@ import { HMSPrebuilt } from "@100mslive/roomkit-react";
 const page = () => {
   return (
     <div style={{ height: "100vh" }}>
-      <HMSPrebuilt roomCode="pli-mvvn-hwi" />
+      <HMSPrebuilt roomCode="rem-koqo-kng" />
     </div>
   );
 };
