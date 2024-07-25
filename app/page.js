9cf323d12aa417bcda05207992ca5327dd09d897
RETDEM LLC. <154280502+RetailDemon@users.noreply.github.com>
2024-07-25 15:39:41 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -197,14 +197,3 @@ export default function Home() {
     </>
   );
 }
-if (document.addEventListener) {
-  document.addEventListener('contextmenu', function(e) {
-    alert("You've tried to open context menu"); //here you draw your own menu
-    e.preventDefault();
-  }, false);
-} else {
-  document.attachEvent('oncontextmenu', function() {
-    alert("You've tried to open context menu");
-    window.event.returnValue = false;
-  });
-}
